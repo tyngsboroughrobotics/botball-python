a23666572ce0a4a1d0cbf23cab0c78f3bbd9395f
Wilson Gramer <wgramer03@gmail.com>
2019-10-24 12:50:19 -0400

    Refactor choose_game_procedure to use RobotConfig helper

--- botball/core/helpers/choose_game_procedure.py
+++ botball/core/helpers/choose_game_procedure.py
@@ -7,11 +7,65 @@ import os
 from ..procedure import Procedure
 
 
+class RobotConfig(object):
+    """
+    Represents the configuration of the target robot controller.
+    """
+
+    robot_type: str
+    """
+    The type of robot targeted. This will either be `"demobot"` or `"create"`.
+    """
+
+    debug_enabled: bool
+    """
+    Whether debugging is enabled on the target robot.
+    """
+
+    def __init__(self):
+        """
+        Create a new instance of `RobotConfig` with the current configuration
+        from the filesystem.
+
+        ## Configuration files
+
+        The following files will affect the configuration:
+
+        - `/etc/ths-botball-conf/demobot`: If this exists, `robot_type` is set
+        to `"demobot"`.
+
+        - `/etc/ths-botball-conf/create`: If this exists, `robot_type` is set to
+        `"create"`.
+
+            - If both of these are set, `demobot` has precedence.
+
+            - If neither of these are set, a `FileNotFoundError` is raised.
+
+        - `/etc/ths-botball-conf/demobot`: If this exists in addition to one of
+        the above, `debug_enabled` is set to `True`.
+        """
+
+        if os.path.exists(f"{RobotConfig.config_path}/demobot"):
+            self.robot_type = "demobot"
+        elif os.path.exists(f"{RobotConfig.config_path}/demobot"):
+            self.robot_type = "create"
+        else:
+            raise FileNotFoundError("Robot controller not configured as Demobot or Create")
+
+        self.debug_enabled = os.path.exists(f"{RobotConfig.config_path}/debug")
+
+    config_path = "/etc/ths-botball-conf"
+    """
+    The path to the configuration files on the robot. You shouldn't need to
+    change this if you use the default project setup.
+    """
+
+
 def choose_game_procedure(demobot: Procedure, create: Procedure, **run_args) -> Callable[[], None]:
     """
     Given two game procedures for a Demobot robot and a iRobot Create robot,
     returns a function that can be called to execute the appropriate procedure
-    with the appropriate settings given the configuration files.
+    with the appropriate settings given the current robot configuration.
 
     - `demobot`: The procedure to execute when running this program on a Demobot
     robot.
@@ -21,36 +75,13 @@ def choose_game_procedure(demobot: Procedure, create: Procedure, **run_args) -> 
 
     - `run_args`: Any additional arguments to pass to the `run` function of the
     chosen procedure.
-
-    ## Configuration files
-
-    The following files will affect the function that is returned:
-
-    - `/etc/ths-botball-conf/demobot`: If this exists, this function returns the
-    `run` function of the `demobot` parameter.
-
-    - `/etc/ths-botball-conf/create`: If this exists, this function returns the
-    `run` function of the `create` parameter.
-
-        - If both of these are set, `demobot` has precedence.
-
-        - If neither of these are set, this function raises a
-        `FileNotFoundError`.
-
-    - `/etc/ths-botball-conf/demobot`: If this exists in addition to one of the
-    above, this function returns the appropriate `run` function with its `debug`
-    parameter set to `True`.
     """
 
-    config_path = "/etc/ths-botball-conf"
+    config = RobotConfig()
 
-    if os.path.exists(f"{config_path}/demobot"):
+    if config.type == "demobot":
         procedure_to_use = demobot
-    elif os.path.exists(f"{config_path}/demobot"):
-        procedure_to_use = create
     else:
-        raise FileNotFoundError("Neither BOTBALL_USE_DEMOBOT nor BOTBALL_USE_CREATE found in environment variables")
+        procedure_to_use = create
 
-    debug = os.path.exists(f"{config_path}/debug")
-
-    return lambda: procedure_to_use.run(debug=debug, **run_args)
+    return lambda: procedure_to_use.run(debug=config.debug_enabled, **run_args)
